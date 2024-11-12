6fc677257a0e53012706a8f634caee3d2d87b330
Fabian <9464524+fbnystn@users.noreply.github.com>
2024-11-12 19:51:35 +0100

    load game when started

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import StartPage from './StartPage';
 import Footer from "./Footer";
+import {GameUtil} from "../game/util/GameUtil";
+import {GameConfigUtil} from "../game/util/GameConfigUtil";
 
 const App: React.FC = () => {
     const [gameStarted, setGameStarted] = useState(false);
@@ -11,12 +13,17 @@ const App: React.FC = () => {
         setPlayerName(playerName);
     };
 
+    useEffect(() => {
+        if (gameStarted) {
+            const game = GameUtil.createGame(GameConfigUtil.createGameConfig(800, 600, "game-container"));
+        }
+    });
+
     return (
         <div className="d-flex flex-column min-vh-100">
             {gameStarted ? (
-                /* TODO replace with game scene */
-                <div className="game-container">
-                    <h2>Game Started for {playerName}!</h2>
+                <div id="game-container" className="game-container">
+                    {/* game content will be rendered here by Phaser */}
                 </div>
             ) : (
                 <StartPage onStart={handleStart}/>
