70a957ac22548474459bce1edb64e1c00c8e81a1
Fabian <9464524+fbnystn@users.noreply.github.com>
2024-11-12 12:35:02 +0100

    added MultiplayerOptionsScene.ts, added Button.ts updated Game.ts, updated MenuScene.ts

--- src/client/game/Game.ts
+++ src/client/game/Game.ts
@@ -1,5 +1,6 @@
 import {BootScene} from "./scenes/BootScene";
 import {MenuScene} from "./scenes/MenuScene";
+import {MultiplayerOptionsScene} from "./scenes/MultiplayerOptionsScene";
 
 export class Game extends Phaser.Game {
     constructor(conf: any) {
@@ -7,6 +8,7 @@ export class Game extends Phaser.Game {
 
         this.scene.add('BootScene', BootScene);
         this.scene.add('MenuScene', MenuScene);
+        this.scene.add('MultiplayerOptionsScene', MultiplayerOptionsScene)
         this.scene.start('BootScene');
     }
 }
--- src/client/game/scenes/MenuScene.ts
+++ src/client/game/scenes/MenuScene.ts
@@ -1,3 +1,5 @@
+import {Button} from "../ui/Button";
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
     visible: false,
@@ -11,217 +13,20 @@ export class MenuScene extends Phaser.Scene {
 
     create() {
         this.add.image(this.cameras.main.width / 2, 150, 'logo');
-        const singlePlayerBtn = this.add.text(this.cameras.main.width / 2, 300, "Singleplayer", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
 
-// Cursor enter the button
-        singlePlayerBtn.on('pointerover', () => {
-            singlePlayerBtn.setStyle({fill: '#ff0'});
-        });
-
-// Event, wenn der Mauszeiger den Button verlässt
-        singlePlayerBtn.on('pointerout', () => {
-            singlePlayerBtn.setStyle({fill: '#ffffff'});
-        });
-        singlePlayerBtn.setInteractive();
-        singlePlayerBtn.on('pointerdown', () => {
+        // Main menu buttons
+        const singlePlayerBtn = new Button(this, this.cameras.main.width / 2, 300, "Singleplayer", () => {
             this.scene.start('GameScene', {});
         });
 
-        const multiPlayerBtn = this.add.text(this.cameras.main.width / 2, 400, "Multiplayer", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
 
-// Cursor enter the button
-        multiPlayerBtn.on('pointerover', () => {
-            multiPlayerBtn.setStyle({fill: '#ff0'});
+        const multiPlayerBtn = new Button(this, this.cameras.main.width / 2, 400, "Multiplayer", () => {
+            this.scene.start('MultiplayerOptionsScene'); // Transition to the multiplayer options scene
         });
 
-// Event, wenn der Mauszeiger den Button verlässt
-        multiPlayerBtn.on('pointerout', () => {
-            multiPlayerBtn.setStyle({fill: '#ffffff'});
-        });
-
-        multiPlayerBtn.setInteractive();
-        multiPlayerBtn.on('pointerdown', () => {
-            singlePlayerBtn.setVisible(false);
-            multiPlayerBtn.setVisible(false);
-            howToPlayBtn.setVisible(false);
-            multiPlayerLocalBtn.setVisible(true);
-            multiPlayerOnlineBtn.setVisible(true);
-            backBtn.setVisible(true);
-        });
-
-        const howToPlayBtn = this.add.text(this.cameras.main.width / 2, 500, "How to Play", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-        howToPlayBtn.setInteractive();
-        howToPlayBtn.on('pointerdown', () => {
+        const howToPlayBtn = new Button(this, this.cameras.main.width / 2, 500, 'How To Play', () => {
             // TODO: implement how to play
         });
-// Cursor enter the button
-        howToPlayBtn.on('pointerover', () => {
-            howToPlayBtn.setStyle({fill: '#ff0'});
-        });
 
-// Event, wenn der Mauszeiger den Button verlässt
-        howToPlayBtn.on('pointerout', () => {
-            howToPlayBtn.setStyle({fill: '#ffffff'});
-        });
-
-        // Multiplayer modes
-        const multiPlayerLocalBtn = this.add.text(this.cameras.main.width / 2, 300, "Lokal", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-        multiPlayerLocalBtn.setInteractive();
-        multiPlayerLocalBtn.on('pointerdown', () => {
-            this.scene.start('GameScene',
-                {})
-        });
-        multiPlayerLocalBtn.setVisible(false);
-// Cursor enter the button
-        multiPlayerLocalBtn.on('pointerover', () => {
-            multiPlayerLocalBtn.setStyle({fill: '#ff0'});
-        });
-
-// Event, wenn der Mauszeiger den Button verlässt
-        multiPlayerLocalBtn.on('pointerout', () => {
-            multiPlayerLocalBtn.setStyle({fill: '#ffffff'});
-        });
-
-        const multiPlayerOnlineBtn = this.add.text(this.cameras.main.width / 2, 400, "Online", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-        multiPlayerOnlineBtn.setInteractive();
-        multiPlayerOnlineBtn.on('pointerdown', () => {
-            this.showMultiplayerOptions();
-        });
-        multiPlayerOnlineBtn.setVisible(false);
-
-// Cursor enter the button
-        multiPlayerOnlineBtn.on('pointerover', () => {
-            multiPlayerOnlineBtn.setStyle({fill: '#ff0'});
-        });
-
-// Event, wenn der Mauszeiger den Button verlässt
-        multiPlayerOnlineBtn.on('pointerout', () => {
-            multiPlayerOnlineBtn.setStyle({fill: '#ffffff'});
-        });
-
-        const backBtn = this.add.text(this.cameras.main.width / 2, 500, "Zurück", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-        backBtn.setInteractive();
-        backBtn.on('pointerdown', () => {
-            singlePlayerBtn.setVisible(true);
-            multiPlayerBtn.setVisible(true);
-            howToPlayBtn.setVisible(true);
-            multiPlayerLocalBtn.setVisible(false);
-            multiPlayerOnlineBtn.setVisible(false);
-            backBtn.setVisible(false);
-        });
-        backBtn.setVisible(false);
-
-// Cursor enter the button
-        backBtn.on('pointerover', () => {
-            backBtn.setStyle({fill: '#ff0'});
-        });
-
-// Event, wenn der Mauszeiger den Button verlässt
-        backBtn.on('pointerout', () => {
-            backBtn.setStyle({fill: '#ffffff'});
-        });
     }
-
-    showMultiplayerOptions() {
-        // Clear the previous buttons (singleplayer and multiplayer buttons)
-        this.children.removeAll();
-
-        // Add text for 'Multiplayer Options'
-        this.add.text(this.cameras.main.width / 2, 150, 'Multiplayer Online', {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-
-        // Host Game Button
-        const hostBtn = this.add.text(this.cameras.main.width / 2, 300, "Host Game", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'
-        }).setOrigin(0.5).setInteractive();
-        hostBtn.setInteractive();
-        hostBtn.on('pointerdown', () => {
-            // TODO: add lobby screen
-            this.scene.start('GameScene', {})
-        });
-
-// Cursor enter the button
-        hostBtn.on('pointerover', () => {
-            hostBtn.setStyle({fill: '#ff0'});
-        });
-
-// Cursor left button
-        hostBtn.on('pointerout', () => {
-            hostBtn.setStyle({fill: '#ffffff'});
-        });
-
-        // Join Game Button
-        const joinBtn = this.add.text(this.cameras.main.width / 2, 400, "Join Game", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'  // Schriftart Lobster Two anwenden
-        }).setOrigin(0.5).setInteractive();
-        joinBtn.setInteractive();
-        joinBtn.on('pointerdown', () => {
-            // TODO: add "enter sessionID" screen -> after join show lobby
-            this.scene.start('GameScene',
-                {})
-        });
-
-// Cursor enter the button
-        joinBtn.on('pointerover', () => {
-            joinBtn.setStyle({fill: '#ff0'});
-        });
-
-// Cursor left button
-        joinBtn.on('pointerout', () => {
-            joinBtn.setStyle({fill: '#ffffff'});
-        });
-
-        // Back Button to go back to main menu
-        const backBtn = this.add.text(this.cameras.main.width / 2, 500, "Back", {
-            fontSize: '30px',
-            color: '#ffffff',
-            fontFamily: '"Lobster Two", cursive'  // Schriftart Lobster Two anwenden
-        }).setOrigin(0.5).setInteractive();
-        backBtn.setInteractive();
-        backBtn.on('pointerdown', () => {
-            this.scene.restart(); // Restart the scene to show the main menu again
-        });
-// Cursor enter the button
-        backBtn.on('pointerover', () => {
-            backBtn.setStyle({fill: '#ff0'});
-        });
-
-// Event, wenn der Mauszeiger den Button verlässt
-        backBtn.on('pointerout', () => {
-            backBtn.setStyle({fill: '#ffffff'});
-        });
-    }
-
-
 }
