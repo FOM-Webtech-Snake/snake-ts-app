ca372539d1aab7c1bbbe764ad5304dfb74b3ede1
Fabian <9464524+fbnystn@users.noreply.github.com>
2024-11-12 19:51:19 +0100

    moved fontawesome import to main.css

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,4 @@
 import '../../public/css/main.css'; // this will apply the css globally
-import '@fortawesome/fontawesome-free/css/all.min.css'; // this will add fontawesome to the app
 import App from "./components/App";
 import ReactDOM from 'react-dom/client';
 import React from 'react';
