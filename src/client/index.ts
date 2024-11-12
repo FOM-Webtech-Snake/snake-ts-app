fc9b8a394bb495efec1c19a0a8793d105436b116
Fabian <9464524+fbnystn@users.noreply.github.com>
2024-11-12 12:33:58 +0100

    added footer.css Footer.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,20 @@
+import '../../public/css/main.css'; // this will apply the css globally
+import '@fortawesome/fontawesome-free/css/all.min.css'; // this will add fontawesome to the app
 import configureClientSocket from "./sockets/socket";
 import {io} from "socket.io-client";
 import config from "./game/GameConfig";
 import {Game} from "./game/Game";
+import {Footer} from "./Footer";
 
-// configure socket
-const socket = io();
-configureClientSocket(socket);
 
-// create game
-const game = new Game(config);
+window.addEventListener('DOMContentLoaded', () => {
+    // configure socket
+    const socket = io();
+    configureClientSocket(socket);
+
+    // create game
+    const game = new Game(config);
+    const footer = new Footer();
+});
+
 
