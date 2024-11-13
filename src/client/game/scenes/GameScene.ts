7313fff0fe330295a2a1a077fe4c1a83f0bd13f0
Fabian <9464524+fbnystn@users.noreply.github.com>
2024-11-13 15:39:30 +0100

    Add player snake and input handling to GameScene
    Introduced a unique player ID, snake instances, and input handlers to the GameScene. The local player snake is now followed by the camera and responds to keyboard input. World bounds and camera constraints are also set up to enhance gameplay.

--- src/client/game/scenes/GameScene.ts
+++ src/client/game/scenes/GameScene.ts
@@ -1,5 +1,10 @@
 import Phaser from "phaser";
 import {Background} from "../ui/Background";
+import {Snake} from "../ui/Snake";
+import UUID = Phaser.Utils.String.UUID;
+import {KeyboardInputHandler} from "../input/KeyboardInputHandler";
+import {InputTypeEnum} from "../../../shared/constants/InputTypeEnum";
+import {InputHandler} from "../input/InputHandler";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: false,
@@ -8,17 +13,46 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class GameScene extends Phaser.Scene {
+
     private background: Background;
+    private playerId: string;
+    private snakes: Record<string, Snake>;
+    private inputHandler: Record<InputTypeEnum, InputHandler>;
+
 
     constructor() {
         super(sceneConfig);
-    }
-
-    preload() {
-
+        this.background = null;
+        this.playerId = UUID(); // create a unique playerId
+        this.snakes = {} as Record<string, Snake>;
+        this.inputHandler = {} as Record<InputTypeEnum, InputHandler>;
     }
 
     create() {
+        // setup world & camera
+        this.physics.world.setBounds(0, 0, 1600, 1200); // push the world bounds to (1600x1200px)
+        this.cameras.main.setBounds(0, 0, 1600, 1200); // setup camera not to leave the world
         this.background = new Background(this);
+
+        // game objects
+        const localSnake = new Snake(this);
+        this.cameras.main.startFollow(localSnake.getHead());
+        this.snakes[this.playerId] = localSnake;
+
+        // input handler
+        const inputHandler = new KeyboardInputHandler(this, localSnake, false);
+        this.inputHandler[inputHandler.getType()] = inputHandler;
+    }
+
+    update() {
+        if (this.inputHandler) {
+            Object.keys(this.inputHandler).forEach(handler => {
+                this.inputHandler[handler].handleInput();
+            })
+        }
+
+        if (this.snakes[this.playerId]) {
+            this.snakes[this.playerId].update();
+        }
     }
 }
