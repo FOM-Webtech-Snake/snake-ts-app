90012e18c03aae362d835ac021decef2b99e4d0a
fbnystn <9464524+fbnystn@users.noreply.github.com>
2024-11-25 17:43:51 +0100

    made world and self collision configurable in game session

--- src/shared/GameSessionConfig.ts
+++ src/shared/GameSessionConfig.ts
@@ -6,10 +6,22 @@ const log = getLogger("shared.GameSessionConfig");
 export class GameSessionConfig {
     private maxPlayers: number;
     private size: Size;
+    private worldCollisionEnabled: boolean;
+    private selfCollisionEnabled: boolean;
 
-    constructor(maxPlayers: number, size: Size) {
+    constructor(maxPlayers: number, size: Size, worldCollisionEnabled: boolean, selfCollisionEnabled: boolean) {
         this.maxPlayers = maxPlayers;
         this.size = size;
+        this.worldCollisionEnabled = worldCollisionEnabled;
+        this.selfCollisionEnabled = selfCollisionEnabled;
+    }
+
+    getWorldCollisionEnabled(): boolean {
+        return this.worldCollisionEnabled;
+    }
+
+    getSelfCollisionEnabled(): boolean {
+        return this.selfCollisionEnabled;
     }
 
     getMaxPlayers(): number {
@@ -24,13 +36,15 @@ export class GameSessionConfig {
         return {
             maxPlayers: this.maxPlayers,
             size: this.size.toJSON(),
+            worldCollisionEnabled: this.worldCollisionEnabled,
+            selfCollisionEnabled: this.selfCollisionEnabled,
         };
     }
 
     static fromData(data: any) {
         log.debug("fromData", data);
-        return new GameSessionConfig(data.maxPlayers, Size.fromData(data.size));
+        return new GameSessionConfig(data.maxPlayers, Size.fromData(data.size), data.worldCollisionEnabled, data.selfCollisionEnabled);
     }
 }
 
-export const DEFAULT_GAME_SESSION_CONFIG = new GameSessionConfig(4, new Size(1600, 1600));
+export const DEFAULT_GAME_SESSION_CONFIG = new GameSessionConfig(4, new Size(1600, 1600), true, false);
