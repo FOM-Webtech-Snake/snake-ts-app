517c085923a1c54cab25113ef4128a0ab42cadeb
fbnystn <9464524+fbnystn@users.noreply.github.com>
2024-11-27 17:40:08 +0100

    footer refactoring

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import "../../../public/css/footer.css"; // Importing CSS
+import "../../../public/css/footer.css";
+import {Col, Container, Row} from "react-bootstrap";
 
 const Footer: React.FC = () => {
     // REPO_URL & BUILD_NUMBER are injected by DefinePlugin
     return (
         <footer>
-            <div className="container">
-                <p>&copy; {new Date().getFullYear()} Snake Extreme (Build: {BUILD_NUMBER})</p>
-                <p>
-                    <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </p>
-            </div>
+            <Container>
+                <Row className="align-items-center">
+                    {/* Left side: Copyright */}
+                    <Col xs={12} md={6}>
+                        <p className="mb-0">&copy; {new Date().getFullYear()} Snake Extreme (Build: {BUILD_NUMBER})</p>
+                    </Col>
+
+                    {/* Right side: GitHub icon */}
+                    <Col xs={12} md={6} className="text-md-end">
+                        <a href={REPO_URL} target="_blank" rel="noopener noreferrer" className="text-white">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     );
 };
