bd87990db5d1e1788af25e9f74b7c824091cbc0a
fbnystn <9464524+fbnystn@users.noreply.github.com>
2024-11-28 12:14:46 +0100

    added configurable start attributes (speed, scale, length and direction)

--- src/shared/model/GameSessionConfig.ts
+++ src/shared/model/GameSessionConfig.ts
@@ -3,6 +3,10 @@ import {getLogger} from "../config/LogConfig";
 
 const log = getLogger("shared.GameSessionConfig");
 
+export const DEFAULT_SNAKE_STARTING_LENGTH: number = 20;
+export const DEFAULT_SNAKE_STARTING_SCALE: number = 0.15;
+export const DEFAULT_SNAKE_STARTING_SPEED: number = 100;
+
 export class GameSessionConfig {
     private maxPlayers: number;
     private size: Size;
@@ -10,12 +14,29 @@ export class GameSessionConfig {
     private selfCollisionEnabled: boolean;
     private playerToPlayerCollisionEnabled: boolean;
 
-    constructor(maxPlayers: number, size: Size, worldCollisionEnabled: boolean, selfCollisionEnabled: boolean, playerToPlayerCollisionEnabled: boolean) {
+    private snakeStartingLength: number;
+    private snakeStartingSpeed: number;
+    private snakeStartingScale: number;
+
+    constructor(
+        maxPlayers: number,
+        size: Size,
+        worldCollisionEnabled: boolean,
+        selfCollisionEnabled: boolean,
+        playerToPlayerCollisionEnabled: boolean,
+        snakeStartingLength: number,
+        snakeStartingSpeed: number,
+        snakeStartingScale: number,
+    ) {
         this.maxPlayers = maxPlayers;
         this.size = size;
         this.worldCollisionEnabled = worldCollisionEnabled;
         this.selfCollisionEnabled = selfCollisionEnabled;
         this.playerToPlayerCollisionEnabled = playerToPlayerCollisionEnabled;
+
+        this.snakeStartingLength = snakeStartingLength;
+        this.snakeStartingSpeed = snakeStartingSpeed;
+        this.snakeStartingScale = snakeStartingScale;
     }
 
     getWorldCollisionEnabled(): boolean {
@@ -38,6 +59,18 @@ export class GameSessionConfig {
         return this.size;
     }
 
+    getSnakeStartingLength(): number {
+        return this.snakeStartingLength;
+    }
+
+    getSnakeStartingSpeed(): number {
+        return this.snakeStartingSpeed;
+    }
+
+    getSnakeStartingScale(): number {
+        return this.snakeStartingScale;
+    }
+
     toJson() {
         return {
             maxPlayers: this.maxPlayers,
@@ -45,13 +78,32 @@ export class GameSessionConfig {
             worldCollisionEnabled: this.worldCollisionEnabled,
             selfCollisionEnabled: this.selfCollisionEnabled,
             playerToPlayerCollisionEnabled: this.playerToPlayerCollisionEnabled,
+            snakeStartingLength: this.snakeStartingLength,
+            snakeStartingSpeed: this.snakeStartingSpeed,
+            snakeStartingScale: this.snakeStartingScale,
         };
     }
 
     static fromData(data: any) {
         log.debug("fromData", data);
-        return new GameSessionConfig(data.maxPlayers, Size.fromData(data.size), data.worldCollisionEnabled, data.selfCollisionEnabled, data.playerToPlayerCollisionEnabled);
+        return new GameSessionConfig(
+            data.maxPlayers,
+            Size.fromData(data.size),
+            data.worldCollisionEnabled,
+            data.selfCollisionEnabled,
+            data.playerToPlayerCollisionEnabled,
+            data.snakeStartingLength,
+            data.snakeStartingSpeed,
+            data.snakeStartingScale);
     }
 }
 
-export const DEFAULT_GAME_SESSION_CONFIG = new GameSessionConfig(4, new Size(1600, 1600), true, false, true);
+export const DEFAULT_GAME_SESSION_CONFIG = new GameSessionConfig(
+    4,
+    new Size(1600, 1600),
+    true,
+    false,
+    true,
+    DEFAULT_SNAKE_STARTING_LENGTH,
+    DEFAULT_SNAKE_STARTING_SPEED,
+    DEFAULT_SNAKE_STARTING_SCALE);
