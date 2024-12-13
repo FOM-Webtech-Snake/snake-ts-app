008d86eb9bc2483da706365e9d5323591863d703
fbnystn <9464524+fbnystn@users.noreply.github.com>
2024-12-13 09:54:37 +0100

    removed rebuild path method -> better positioning with less jitter

--- src/client/game/ui/PhaserSnake.ts
+++ src/client/game/ui/PhaserSnake.ts
@@ -218,7 +218,7 @@ export class PhaserSnake {
         this.updateScaling();
     }
 
-    updateScaling(): void {
+    private updateScaling(): void {
         const bodyParts = this.body.getChildren() as Phaser.Physics.Arcade.Sprite[];
 
         for (let segment of bodyParts) {
@@ -426,8 +426,6 @@ export class PhaserSnake {
         this.score = player.getScore();
         this.direction = player.getDirection();
 
-        this.path = this.rebuildPath(player.getBodyPositions());
-
         // update body parts to match the new positions
         const bodyParts = this.body.getChildren() as Phaser.GameObjects.Sprite[];
         player.getBodyPositions().forEach((segment, index) => {
@@ -468,49 +466,6 @@ export class PhaserSnake {
             log.trace("updating color from player", player);
             this.setPrimaryColor(ColorUtil.rgbToHex(player.getColor()));
         }
-    }
-
-    private rebuildPath(newBodyPositions: Position[]): Phaser.Math.Vector3[] {
-        if (newBodyPositions.length < 1) {
-            return [];
-        }
-
-        const segmentWidth = Math.ceil(this.head.displayWidth);
-        const newPath: Phaser.Math.Vector3[] = [];
-
-        for (let i = 0; i < newBodyPositions.length; i++) {
-            const currentSegment = newBodyPositions[i];
-            const start = new Phaser.Math.Vector3(
-                currentSegment.getX(),
-                currentSegment.getY(),
-                currentSegment.getRotation()
-            );
-            newPath.push(start);
-
-            if (i < newBodyPositions.length - 1) {
-                const nextSegment = newBodyPositions[i + 1];
-                const end = new Phaser.Math.Vector3(
-                    nextSegment.getX(),
-                    nextSegment.getY(),
-                    nextSegment.getRotation()
-                );
-
-                // calculate interpolation
-                const distance = Phaser.Math.Distance.Between(start.x, start.y, end.x, end.y);
-                const steps = Math.floor(distance / segmentWidth);
-
-                for (let t = 1; t < steps; t++) {
-                    const ratio = t / steps;
-                    const interpolatedX = Phaser.Math.Linear(start.x, end.x, ratio);
-                    const interpolatedY = Phaser.Math.Linear(start.y, end.y, ratio);
-                    const interpolatedZ = Phaser.Math.Linear(start.z, end.z, ratio);
-
-                    newPath.push(new Phaser.Math.Vector3(interpolatedX, interpolatedY, interpolatedZ));
-                }
-            }
-        }
-
-        return newPath;
     }
 
     private appendSegmentsByPositions(startPosIdx: number, positions: Position[]) {
